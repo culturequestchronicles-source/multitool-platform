e29f9219f6f7dd394634932f255bc977f6707510
culturequestchronicles-source <culturequestchronicles@gmail.com>
2025-12-28 19:24:40 -0500

    Move outputFileTracingIncludes to root

---
+++ types/pdfjs-worker.d.ts
@@ -0,0 +1,13 @@
+declare module "pdfjs-dist/legacy/build/pdf.worker.mjs" {
+  const workerModule: {
+    WorkerMessageHandler: unknown;
+  };
+  export = workerModule;
+}
+
+declare module "pdf-parse/node_modules/pdfjs-dist/legacy/build/pdf.worker.mjs" {
+  const workerModule: {
+    WorkerMessageHandler: unknown;
+  };
+  export = workerModule;
+}
